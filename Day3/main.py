c36614d2e3ec6829e0150997e2a8044fed9f0ed0
KeefeT <tjk819@gmail.com>
2023-12-05 06:15:55 -0700

    going to work

---
+++ Day3/main.py
@@ -0,0 +1,128 @@
+
+def main():
+    file = open("input.txt", "r")
+    line = file.readline()
+    sum = 0
+    # array of strings
+    arr = []
+
+    while line:
+        arr.append(line)
+        line = file.readline()
+
+    nums = check_adjacent(arr)
+    
+    for num in nums:
+        sum += num
+
+    print(nums)
+    print(sum)
+        
+
+    file.close()
+
+
+def check_adjacent(arr):
+    
+    nums_to_sum = []
+    num = ''
+    inNum = False
+    isTouching = False
+    touched = False
+
+    for y in range(0, len(arr)):
+        for x in range(0, len(arr[-1])):
+            # print(f'{x},{y} {arr[y][x]}')
+
+            inNum = (arr[y][x].isdigit())
+            isTouching = calculateTouching(x, y, arr)
+                    
+            if inNum:
+                if isTouching:
+                    touched = True
+                num += arr[y][x]
+                isTouching = False
+            elif num != '':
+                if touched:
+                    nums_to_sum.append(int(num))
+                num = ''
+                touched = False
+
+        if num !=  '':
+            if touched:
+                nums_to_sum.append(int(num))
+            num = ''
+            touched = False
+
+    return nums_to_sum
+
+
+
+def calculateTouching(x, y, arr):
+    isTouching = False
+
+    try:
+        isTouching = (arr[y-1][x].isdigit() == False and arr[y-1][x] != '.')
+    except:
+        None
+
+    if isTouching:
+        return isTouching
+
+    try:
+        isTouching = (arr[y-1][x-1].isdigit() == False and arr[y-1][x-1] != '.')
+    except:
+        None
+
+    if isTouching:
+        return isTouching
+
+    try:
+        isTouching = (arr[y-1][x+1].isdigit() == False and arr[y-1][x+1] != '.')
+    except:
+        None
+
+    if isTouching:
+        return isTouching
+
+    try:
+        isTouching = (arr[y+1][x].isdigit() == False and arr[y+1][x] != '.')
+    except:
+        None
+
+    if isTouching:
+        return isTouching
+
+    try:
+        isTouching = (arr[y+1][x-1].isdigit() == False and arr[y+1][x-1] != '.')
+    except:
+        None
+
+    if isTouching:
+        return isTouching
+
+    try:
+        isTouching = (arr[y+1][x+1].isdigit() == False and arr[y+1][x+1] != '.')
+    except:
+        None
+
+    if isTouching:
+        return isTouching
+
+    try:
+        isTouching = (arr[y][x+1].isdigit() == False and arr[y][x+1] != '.')
+    except:
+        None
+
+    if isTouching:
+        return isTouching
+
+    try:
+        isTouching = (arr[y][x-1].isdigit() == False and arr[y][x-1] != '.')
+    except:
+        None
+
+    return isTouching
+
+if __name__ == '__main__':
+    main()
